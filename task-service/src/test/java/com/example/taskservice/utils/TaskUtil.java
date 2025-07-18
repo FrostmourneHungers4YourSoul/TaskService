34de915ddb063ab204ececc5f72367d6d024ed43
Aidar <tls.gamer78@gmail.com>
2025-07-18 12:47:06 +0600

    Task Service: delete credentials

---
+++ task-service/src/test/java/com/example/taskservice/utils/TaskUtil.java
@@ -0,0 +1,9 @@
+package com.example.taskservice.utils;
+
+import com.example.taskservice.model.Task;
+
+public class TaskUtil {
+    public static Task createTask() {
+        return null;
+    }
+}
